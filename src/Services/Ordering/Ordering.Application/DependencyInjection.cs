67f7efa23f8fe593c44fcd1adc183a38aff5c938
juanw87 <ba.juanjose@gmail.com>
2024-11-30 17:43:43 -0300

    Sección 13:  Ordering Microservices with DDD, CQRS, and Clean Architecture

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services) 
+        {
+            //services.AddMediatR(cfg => {
+            //    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            //});
+
+            return services;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infraestructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infraestructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraestructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            //// Add services to the container.
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(connectionString));
+
+            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
