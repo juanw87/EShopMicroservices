a7435bf2fec64f02c0d35d6df6bd9a18f759041d
juanw87 <ba.juanjose@gmail.com>
2024-12-06 17:50:58 -0300

    Sección 14: Ordering.Domain Layer with Tactical Domain-Driven Design Patterns

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IAggregate.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Domain.Abstractions
+{
+    internal interface IAggregate<T> : IAggregate, IEntity<T>
+    {
+
+    }
+
+    internal interface IAggregate : IEntity
+    {
+        IReadOnlyList<IDomainEvent> DomainEvents { get; }
+        IDomainEvent[] ClearDomainEvents();
+    }
+}
